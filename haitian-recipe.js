35099efa13190700cbe710d1476fc742e6a76559
LynnsDoesCoding <lylybaeee@gmail.com>
2026-05-21 22:49:24 -0400

    Delete haitian-recipe.js

--- haitian-recipe.js
+++
@@ -1,7 +0,0 @@
-addRecipe("rice+bean+oil+garlic+onion", "Diri Kole");
-
-addRecipe("pumpkin+beef+carrot+cabbage+pasta", "Soup Joumou");
-
-addRecipe("flour+sugar+milk+oil", "Beignet");
-
-addRecipe("fish+lemon+garlic+onion+oil", "Poisson Rouge");
