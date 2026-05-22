44ff4c9c3723f0a88ffa0dedfbae7d703f9f6ace
LynnsDoesCoding <lylybaeee@gmail.com>
2026-05-21 22:35:39 -0400

    Create haitian-recipe.js

---
+++ haitian-recipe.js
@@ -0,0 +1,7 @@
+addRecipe("rice+bean+oil+garlic+onion", "Diri Kole");
+
+addRecipe("pumpkin+beef+carrot+cabbage+pasta", "Soup Joumou");
+
+addRecipe("flour+sugar+milk+oil", "Beignet");
+
+addRecipe("fish+lemon+garlic+onion+oil", "Poisson Rouge");
